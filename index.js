ae08ba82dc126ac93c9a9166cf8835e0702c0e37
Matti Lankinen <m.lankinen@iki.fi>
2015-03-28 00:25:41 +0200

    Add gulp notify support

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 var plugin     = require('./lib/browserify/plugin'),
-    listen     = require('./lib/server/listen');
+    listen     = require('./lib/server/listen'),
+    notify     = require('./lib/server/notify'),
+    gulpnofify = require('./lib/gulp/notify');
 
 module.exports = plugin;
 
+module.exports.listen = listen;
+module.exports.notify = notify;
+module.exports.gulpnotify = gulpnofify
