236ca25b54ebd2382d22b0133acc0c4acc9bf94a
Matti Lankinen <m.lankinen@iki.fi>
2015-03-28 00:23:52 +0200

    Add browserify plugin code

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 'use strict';
 
-var transform = require('./transform/transform'),
-    notify    = require('./transform/notify'),
-    listen    = require('./transform/listen'),
-    prefix    = require('./client/prefix'),
-    suffix    = require('./client/suffix')
+var plugin     = require('./lib/browserify/plugin');
 
+module.exports = plugin;
 
-module.exports = transform
-module.exports.notify = notify
-module.exports.listen = listen
-module.exports.prefix = prefix
-module.exports.suffix = suffix
