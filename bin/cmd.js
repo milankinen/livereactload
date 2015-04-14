d4a22ac8941ff41b6ca9a3faf66310d5c591d02f
Matti Lankinen <m.lankinen@iki.fi>
2015-04-14 11:14:10 +0300

    Replace old scripts with yargs implementation

---
+++ bin/cmd.js
@@ -0,0 +1,67 @@
+var yargs = require('yargs')
+
+var rootYargs = yargs
+  .usage('Usage: $0 <command> [options]')
+  .command('listen', 'Starts LiveReactload server that listen for notifications and sends reload events to the clients')
+  .demand(1)
+
+
+var root = rootYargs.argv,
+    cmd  = root._[0]
+
+if (cmd === 'listen') {
+
+  var listenYargs = yargs
+    .usage('Usage: $0 listen [--help|-h] [reload-port] [notify-port]')
+    .alias('h', 'help')
+    .describe('h', 'Display help')
+    .epilogue('reload-port = Reloading port that listens new WebSocket clients (browsers)\n' +
+              'notify-port = HTTP port that can be used to send notifications')
+
+  var listen = listenYargs.argv
+  if (listen.h) {
+    listenYargs.showHelp()
+  } else {
+    require('../lib/server/listen')({port: listen._[1], notifyPort: listen._[2]})
+  }
+
+} else if (cmd === 'notify') {
+
+  var notifyYargs = yargs
+    .usage('Usage: $0 notify [--help|-h] [notify-port] [host]')
+    .alias('h', 'help')
+    .describe('h', 'Display help')
+    .epilogue('notify-port = Notification HTTP port (see "listen" command) that triggers reloading event\n' +
+              'host        = Host that listens for notifications. Defaults to localhost')
+
+  var notify = notifyYargs.argv
+  if (notify.h) {
+    notifyYargs.showHelp()
+  } else {
+    require('../lib/server/notify')(notify._[2], notify._[1], function() {
+      process.exit(0)
+    })
+  }
+
+} else if (cmd === 'monitor') {
+
+  var monitorYargs = yargs
+    .usage('Usage: $0 monitor [--help|-h] [--show-notification|-n] <bundle-path>')
+    .example('$0 monitor public/bundle.js')
+    .boolean(['n', 'h'])
+    .alias('h', 'help')
+    .describe('h', 'Display help')
+    .alias('n', 'show-notification')
+    .describe('n', 'Display a desktop notification every time when bundle changes')
+    .epilogue('Starts monitoring the given bundle file changes and sends a reloading event every time when change occurs')
+
+  var monitor = monitorYargs.argv
+  if (monitor.h || monitor._.length < 2) {
+    monitorYargs.showHelp()
+  } else {
+    require('../lib/server/monitor')(monitor._[1], {displayNotification: !!monitor.n})
+  }
+
+} else {
+  rootYargs.showHelp()
+}
--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 var transform  = require('./lib/browserify/transform'),
     listen     = require('./lib/server/listen'),
     notify     = require('./lib/server/notify'),
+    monitor    = require('./lib/server/monitor'),
     gulpnofify = require('./lib/gulp/notify');
 
 module.exports = transform;
 
 module.exports.listen = listen;
 module.exports.notify = notify;
-module.exports.gulpnotify = gulpnofify
+module.exports.gulpnotify = gulpnofify;
+module.exports.monitor = monitor;
