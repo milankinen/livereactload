218b2ab4e645f3b667fdf4cce77a8638ca18c982
Matti Lankinen <m.lankinen@iki.fi>
2015-09-16 02:04:24 +0300

    Add very simple babel react transform

--- src/babel-plugin/main.js
+++ src/babel-plugin/main.js
@@ -1,1 +1,24 @@
+const {getForceUpdate, createProxy} = require("react-proxy")
 
+export default function babelPluginLiveReactload({filename, components, imports, locals}) {
+  console.log(filename, components, imports, locals)
+  const [React] = imports
+  const forceUpdate = getForceUpdate(React)
+
+  return function applyProxy(Component, uniqueId) {
+    const id = filename + "$$" + uniqueId
+    console.log("apply", filename, id)
+
+    const proxies = window.__reactProxies = window.__reactProxies || {}
+    if (!proxies[id]) {
+      const proxy = createProxy(Component)
+      proxies[id] = proxy
+      return proxy.get()
+    } else {
+      const proxy = proxies[id]
+      const instances = proxy.update(Component)
+      setTimeout(() => instances.forEach(forceUpdate), 0)
+      return proxy.get()
+    }
+  }
+}
