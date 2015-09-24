e60ef4262ebbeeabe330a0ee411aebb5575da172
Matti Lankinen <m.lankinen@iki.fi>
2015-09-24 21:52:20 +0300

    Fix IntelliJ redux syntax error

--- examples/02-redux/reducers/counter.js
+++ examples/02-redux/reducers/counter.js
@@ -1,6 +1,6 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
 
-export default function counter(state = 0, action) {
+export default function counter(state = 0, action = {}) {
   switch (action.type) {
   case INCREMENT_COUNTER:
     return state + 1;
