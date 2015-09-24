cfc380d646b3e63e59cc0b53bb99c3bb6c2ed759
Matti Lankinen <m.lankinen@iki.fi>
2015-09-24 21:50:27 +0300

    Fix redux example increment value

--- examples/02-redux/reducers/counter.js
+++ examples/02-redux/reducers/counter.js
@@ -3,7 +3,7 @@ import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../actions/counter';
 export default function counter(state = 0, action) {
   switch (action.type) {
   case INCREMENT_COUNTER:
-    return state + 5;
+    return state + 1;
   case DECREMENT_COUNTER:
     return state - 1;
   default:
