2919896e5d0606867b997db1b256ca2daa688a40
Matti Lankinen <m.lankinen@iki.fi>
2015-10-18 13:40:38 +0300

    Fix redux example to accept “configureStore.js” module

--- examples/02-redux/store/configureStore.js
+++ examples/02-redux/store/configureStore.js
@@ -9,15 +9,16 @@ const createStoreWithMiddleware = applyMiddleware(
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(reducer, initialState);
 
-  // When using WebPack, module.hot.accept should be used. In
-  // LiveReactload, same result can be achieved by using
-  // module.onReload hook. This hook doesn't prevent reloading
-  // propagation but at least it gives modules a change to react
-  // to reloading events
+  // When using WebPack, module.hot.accept should be used. In LiveReactload,
+  // same result can be achieved by using "module.onReload" hook.
   if (module.onReload) {
     module.onReload(() => {
       const nextReducer = require('../reducers');
       store.replaceReducer(nextReducer);
+
+      // return true to indicate that this module is accepted and
+      // there is no need to reload its parent modules
+      return true
     });
   }
 
