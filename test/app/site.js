8c1979c70b07aab74017d146d9088b5965f145c3
Matti Lankinen <m.lankinen@iki.fi>
2016-01-28 13:42:45 +0200

    Update test app dependencies

--- test/app/site.js
+++ test/app/site.js
@@ -1,5 +1,6 @@
-const React = require("react"),
-      App   = require("./.src/app")
+import React from "react"
+import { render } from "react-dom"
+import App from "./.src/app"
 
 // we are requiring redux here even though it's not used because
 // redux 3.0.0 contains some dependencies that are de-duplicated
@@ -7,8 +8,6 @@ const React = require("react"),
 // of these bugs caught by adding this require (the tests get broken
 // when dedupe handling gets broken)
 require("redux")
-
-const {render} = require("react-dom")
 
 console.log("Increment site.js reload counter...")
 window._siteReloadCounter = "_siteReloadCounter" in window ? window._siteReloadCounter + 1 : 0
