96b3b536ae5f082fd4feec8f98ae53f051df2eda
Matti Lankinen <m.lankinen@iki.fi>
2016-07-11 01:02:35 +0300

    Remove old reloading logic

--- src/browserify-plugin/makeHash.js
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto"
-
-export default function calculateHash(sources) {
-  const md5sum = crypto.createHash('md5')
-  md5sum.update(sources)
-  return md5sum.digest('hex')
-}
