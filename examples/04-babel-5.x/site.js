8512d16ed978d6c58cd0f16332abe02918f94048
Matti Lankinen <m.lankinen@iki.fi>
2016-07-11 03:38:03 +0300

    Remove Babel 5.x example

--- examples/04-babel-5.x/site.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import {render} from "react-dom"
-import App from "./src/app"
-
-const initialModel = window.INITIAL_MODEL
-
-render(<App {...initialModel} />, document.getElementById("app"))
