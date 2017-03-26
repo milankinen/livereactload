35112aa5323475b6e72b7781b4347da92de69041
Matti Lankinen <m.lankinen@iki.fi>
2017-03-27 00:20:09 +0300

    Update and fix examples to use beta package

--- examples/01-basic-usage/site.js
+++ examples/01-basic-usage/site.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {render} from "react-dom"
-import {AppContainer} from "babel-hot-loader"
+import {AppContainer} from "react-hot-loader"
 import App from "./src/app"
 
 const initialModel = window.INITIAL_MODEL
