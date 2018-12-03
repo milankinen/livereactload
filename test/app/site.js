14fd5a69bb4bb242238c11bf52ed0149983c3405
Matti Lankinen <m.lankinen@iki.fi>
2018-12-03 23:14:07 +0200

    Fix tests

--- test/app/site.js
+++ test/app/site.js
@@ -1,34 +1,30 @@
-import React from "react"
-import { render } from "react-dom"
-import App from "./.src/app"
-
-try {
-  window._clockMaker = require('clockmaker');
-} catch (err) {
-  console.warn('Clockmaker not found!');
-}
+import React from "react";
+import { render } from "react-dom";
+import App from "./.src/app";
 
 // we are requiring redux here even though it's not used because
 // redux 3.0.0 contains some dependencies that are de-duplicated
 // by Browserify, thus breaking all the time -- we can get most
 // of these bugs caught by adding this require (the tests get broken
 // when dedupe handling gets broken)
-require("redux")
+require("redux");
 
-
-console.log("Increment site.js reload counter...")
-window._siteReloadCounter = "_siteReloadCounter" in window ? window._siteReloadCounter + 1 : 0
-
+console.log("Increment site.js reload counter...");
+window._siteReloadCounter =
+  "_siteReloadCounter" in window ? window._siteReloadCounter + 1 : 0;
 
 const MyApp = React.createClass({
   render() {
-    return window._siteReloadCounter === 0 ? <App /> : (
+    return window._siteReloadCounter === 0 ? (
+      <App />
+    ) : (
       <div>
         This text should never occur because LiveReactLoad should stop reload
-        propagation if module returns only React components (like src/app.js does).
+        propagation if module returns only React components (like src/app.js
+        does).
       </div>
-    )
+    );
   }
-})
+});
 
-render(<MyApp />, document.getElementById("app"))
+render(<MyApp />, document.getElementById("app"));
--- test/utils.js
+++ test/utils.js
@@ -1,54 +1,55 @@
-import Promise from "bluebird"
-import chokidar from "chokidar"
-import fs from "fs"
-import {resolve} from "path"
-import * as sh from "shelljs"
+import Promise from "bluebird";
+import chokidar from "chokidar";
+import fs from "fs";
+import { resolve } from "path";
+import * as sh from "shelljs";
 
 export function startServer() {
-  execInApp("mkdir -p .src && rm -rf .src/* && cp -R src/* .src")
-  execInApp("mkdir -p node_modules && rm -rf node_modules/livereactload")
-  execInApp("npm i")
-  execInApp("npm run bundle:vendor")
-  return execInApp("npm start", {async: true})
+  execInApp("mkdir -p .src && rm -rf .src/* && cp -R src/* .src");
+  //execInApp("mkdir -p node_modules && rm -rf node_modules/livereactload")
+  //execInApp("npm i")
+  execInApp("npm run bundle:vendor");
+  return execInApp("npm start", { async: true });
 }
 
-
 export function execInApp(cmd, opts) {
-  return sh.exec(`cd ${resolve(__dirname, "app")} && ${cmd}`, opts)
+  return sh.exec(`cd ${resolve(__dirname, "app")} && ${cmd}`, opts);
 }
 
 export function wait(time) {
-  return new Promise(resolve => setTimeout(resolve, time))
+  return new Promise(resolve => setTimeout(resolve, time));
 }
 
 export function updateSources(browser, files) {
-  const promises = files.map(({file, find, replace}) =>
-    new Promise((res, rej) => {
-      const filename = resolve(__dirname, "app/.src", file)
-      const watcher = chokidar.watch(filename, {
-        persistent: false,
-        usePolling: true,
-        interval: 100
-      })
+  const promises = files.map(
+    ({ file, find, replace }) =>
+      new Promise((res, rej) => {
+        const filename = resolve(__dirname, "app/.src", file);
+        const watcher = chokidar.watch(filename, {
+          persistent: false,
+          usePolling: true,
+          interval: 100
+        });
 
-      watcher.on("change", () => {
-        watcher.close()
-        res()
+        watcher.on("change", () => {
+          watcher.close();
+          res();
+        });
+        watcher.on("error", e => {
+          watcher.close();
+          rej(e);
+        });
+        watcher.on("ready", () => {
+          console.log("Update source file", file);
+          const content = fs.readFileSync(filename).toString();
+          const newContent = content.replace(find, replace);
+          fs.writeFileSync(filename, newContent);
+        });
       })
-      watcher.on("error", e => {
-        watcher.close()
-        rej(e)
-      })
-      watcher.on("ready", () => {
-        console.log("Update source file", file)
-        const content = fs.readFileSync(filename).toString()
-        const newContent = content.replace(find, replace)
-        fs.writeFileSync(filename, newContent)
-      })
-    }))
+  );
 
   return Promise.all(promises)
     .timeout(1000)
     .then(() => wait(1000))
-    .then(() => browser.wait(100))
+    .then(() => browser.wait(100));
 }
